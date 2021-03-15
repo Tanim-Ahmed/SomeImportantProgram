43bcab339c844118f6c20c478946cd18a92b54ad
Tanim Ahmed <tanim.ahmed.oct@gmail.com>
2021-03-16 00:18:55 +0600

    Change

--- main.cpp
+++ main.cpp
@@ -1,37 +1,37 @@
-#include <iostream>
+#include<iostream>
 #include<cmath>
 using namespace std;
-
 int main()
 {
+ int num,res=0,n=0,poww,rem;
+ cout<<"Enter your number"<<endl;
+ cin >> num;
+ int orgn;
+ orgn = num;
+ //digit count
+ while(orgn!=0)
+ {
+     orgn/=10;
+     n++;
+ }
+  orgn = num;
+  while(orgn!=0)
+  {
+      rem = orgn%10;
+      poww=round(pow(rem,n));
+      res+=poww;
+      orgn/=10;
+  }
+  if(res==num)
+  {
+      cout<<"Armstrong Number"<<endl;
+  }
+  else
+  {
+      cout<<"Not Armstrong Number"<<endl;
+  }
 
-    int num,orgn,rem,n=0,res=0,poww;
-    cin >> num;
-    orgn = num;
-    while(orgn!=0)
-    {
-        orgn/=10;
-        ++n;
-    }
-    orgn = num;
-    while(orgn!=0)
-    {
-        rem = orgn %10;
-        poww=round(pow(rem,n));
-        res+=poww;
-        orgn/=10;
 
-    }
-    if(res==num)
-    {
-        cout<<"Armstrong Number"<<endl;
-    }
-    else
-    {
-        cout<<"Not armstrong Number"<<endl;
-    }
+ return 0;
 
-
-
-    return 0;
 }
