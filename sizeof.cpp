2ea4eaae4da6220a31a3f8df6a65822210dc0f1e
Tanim Ahmed <tanim.ahmed.oct@gmail.com>
2021-03-16 00:30:03 +0600

    Sizeof different opeartor

---
+++ sizeof.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    long double a = 454534.34343;
+    cout<<"Sizeof Different data types"<<endl;
+    cout<<"Integer:"<<sizeof(1)<<endl;
+    cout<<"Char:"<<sizeof('a')<<endl;
+    cout<<"Float:"<<sizeof(65.34)<<endl;
+    cout<<"String"<<sizeof("Tnaim")<<endl;
+    cout<<"Long Double"<<sizeof(a)<<endl;
+    return 0;
+}
