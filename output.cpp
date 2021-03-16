292431ff1e341b3347eb2c10034a04d5308f5034
Tanim Ahmed <tanim.ahmed.oct@gmail.com>
2021-03-16 23:48:40 +0600

    Some output Program

---
+++ output.cpp
@@ -0,0 +1,86 @@
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i;
+    for(i=0;i<5;i++);
+    cout<<i<<endl;
+    //output 5
+
+
+
+
+    return 0;
+}
+
+#include<cstdio>
+#include<iostream>
+using namespace std;
+int main(){
+
+    int x=1234;
+    int i={
+         printf("c" "++")
+    };
+    for(x=0;x<=i;x++){
+         printf("%x ",x);
+    }
+
+
+   char c=125;
+   do
+
+         printf("%d ",c);
+    while(c++);
+
+   return 0;
+}
+
+#include<iostream>
+using namespace std;
+int main()
+{
+int a = 5,b = 2,c = 1;
+if(a&&b<c){
+    cout<<"Bangladesh"<<endl;
+}
+else
+{
+    cout<<"Tanim";
+}
+
+
+return 0;
+}
+
+#include<iostream>
+#include<cstdio>
+#include<cstdlib>
+using namespace std;
+int main()
+{
+    char str[120]="Digital Bangladesh";
+    int n;
+    n = strlen(str);
+    cout<<n<<endl;
+    return 0;
+}
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i;
+    for(i = 0;i<5;i++)
+    {
+
+        if(i==2)
+
+
+            continue;
+             cout<<i<<endl;
+
+    }
+    return 0;
+}
